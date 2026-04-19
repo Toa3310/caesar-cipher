4b3773bbc1c112ed8509e6f6c51fc7da40488373
Toa Yamamoto <241205182@ccmailg.meijo-u.ac.jp>
2026-04-20 03:50:04 +0900

    initial commit

---
+++ caesar.py
@@ -0,0 +1,31 @@
+def apply_mode(mode, shift):
+    return shift if mode == "1" else -shift
+
+def char_shift(char, shift):
+    if(char.isalpha()):
+        base = ord('A') if char.isupper() else ord('a')
+        shifted_char = chr((ord(char) - base + shift) % 26 + base)
+        return  shifted_char
+    else:
+        return char
+
+def caesar_cipher(mode, text, shift):
+    applied_shift = apply_mode(mode, shift)
+    result = ""
+    for char in text:
+        result += char_shift(char, applied_shift)
+    return result
+
+mode = input("モードを選択してください(暗号化:1, 復号:2) → ")
+if(mode == "1"):
+    plaintext = input("平文を入力してください: ")
+    shift = int(input("シフト量を入力してください（例: 3）: "))
+    encryptedtext = caesar_cipher(mode, plaintext, shift)
+    print("暗号文:", encryptedtext)
+elif(mode == "2"):
+    ciphertext = input("暗号文を入力してください: ")
+    shift = int(input("シフト量を入力してください (例: 3) : "))
+    decryptedtext = caesar_cipher(mode, ciphertext, shift)
+    print("復号結果:", decryptedtext)
+else:
+    print("入力が間違っています。")
