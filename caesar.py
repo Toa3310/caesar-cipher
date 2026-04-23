8ba2da28bda73042fd14a9c87e77b371e858e06e
Toa Yamamoto <241205182@ccmailg.meijo-u.ac.jp>
2026-04-24 01:36:38 +0900

    例外処理の追加、今後のタスクをREADMEに追加

--- caesar.py
+++ caesar.py
@@ -1,6 +1,18 @@
 def apply_mode(mode, shift):
     return shift if mode == "1" else -shift
 
+def input_shift():
+    count = 0
+    while count < 5:
+        try:
+            shift =  int(input("シフト量を入力してください（例: 3）: "))
+            return shift
+        except ValueError:
+            print("整数以外が入力されました。もう一度入力してください")
+            count += 1
+    print("5回連続で入力ミスがありました。")
+    return None
+
 def char_shift(char, shift):
     if char.isalpha():
         base = ord('A') if char.isupper() else ord('a')
@@ -17,12 +29,20 @@ def caesar_cipher(mode, text, shift):
 if __name__ == "__main__":
     mode = input("モードを選択してください(暗号化:1, 復号:2) → ")
     if mode == "1":
-        plaintext = input("平文を入力してください: ")
-        shift = int(input("シフト量を入力してください（例: 3）: "))
-        print("暗号文:", caesar_cipher(mode, plaintext, shift))
+        text = input("平文を入力してください: ")
     elif mode == "2":
-        ciphertext = input("暗号文を入力してください: ")
-        shift = int(input("シフト量を入力してください (例: 3) : "))
-        print("復号結果:", caesar_cipher(mode, ciphertext, shift))
+        text = input("暗号文を入力してください: ")
     else:
-        print("入力が間違っています。")
+        print("モード選択では1か2を入力してください。")
+        exit()
+
+    shift = input_shift()
+    if shift is None:
+        print("処理を終了します。")
+        exit()
+
+    result = caesar_cipher(mode, text, shift)
+    if mode == "1":
+        print("暗号文:", result)
+    else:
+        print("復号結果:", result)
