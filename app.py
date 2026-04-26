0c7656b01c9976db8e2bc601ef7377dcc5d58b75
Toa Yamamoto <241205182@ccmailg.meijo-u.ac.jp>
2026-04-27 00:58:11 +0900

    web上でシフト数未入力の場合の処理を追加

--- app.py
+++ app.py
@@ -6,12 +6,22 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = ""
+    mode_option = ""
     if request.method == "POST":
-        text = request.form["text"]
-        shift = int(request.form["shift"])
-        mode = request.form["mode"]
-        result = caesar_cipher(mode, text, shift)
-    return render_template("index.html", result=result)
+        try:
+            text = request.form["text"]
+            shift = int(request.form["shift"])
+            mode = request.form["mode"]
+            result = caesar_cipher(mode, text, shift)
+
+            if mode == "1":
+                mode_option = "暗号化結果"
+            elif mode == "2":
+                mode_option = "復号結果"
+        except ValueError:
+            result = "エラー(シフト数が入力されていません)"
+            mode_option = "処理結果"
+    return render_template("index.html", result=result, mode_label=mode_option)
 
 if __name__ == "__main__":
     app.run(debug=True)
